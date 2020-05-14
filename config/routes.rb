73e875089183c9e4315752121c897da713c92dfc
nzyumek <nzyume.k@gmail.com>
2020-05-14 06:22:33 +0000

    finish inquiry form in about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'inquiry' => 'inquiry#index'
+  #get 'inquiry' => 'inquiry#index'
   post 'inquiry/confirm' => 'inquiry#confirm'
   post 'inquiry/thanks' => 'inquiry#thanks'
   
