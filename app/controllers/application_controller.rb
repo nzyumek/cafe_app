17c5af52d5bd0d0f9a04e39a7bfc07c61dc37604
nzyumek <nzyume.k@gmail.com>
2020-05-31 03:41:17 +0000

    changes in scss 1241 0531

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :store_user_location!, if: :storable_location?
 
-    
+
     
     protected
     def configure_permitted_parameters
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,14 @@ class HomeController < ApplicationController
     def index
         # @cafe_lists = CafeList.order('id DESC').limit(3)
         # @cafe_lists = CafeList.order('id DESC').first(3)
-        @cafe_lists = CafeList.last(3)
-        @cafe_lists_carousel = CafeList.all.sample(3)
+        @cafe_lists_card = CafeList.all.sample(3)
+        
+        params[:title].blank? ? params[:title]="" : params[:title]
+        params[:bean].blank? ? params[:bean]="" : params[:bean]
+        params[:location].blank? ? params[:location]="" : params[:location]
+        
+        @cafe_lists = CafeList.where("title LIKE ? OR bean LIKE ? OR location LIKE ?", "%" + params[:title] + "%", "%" + params[:bean] + "%", "%" + params[:location] + "%")
     end
+    
     
 end
